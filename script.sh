0a0e1b9568ff928580a1c70d12e2f3b1d6d94295
Cristian Salvi <5alv1.py@Cristians-Air.lan>
2022-01-24 22:31:34 +0100

    deleted useless stuff

--- script.sh
+++
@@ -1,8 +0,0 @@
-echo "# png" >> README.md
-git init
-git add README.md
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Salvi03/pngUtils.git
-git push -u origin main
