619799076d936a178522ac314ef0a4ecfb7a21b4
Cristian Salvi <5alv1.py@Cristians-MacBook-Air.local>
2022-01-25 09:34:39 +0100

    commit

--- reader.go
+++ reader.go
@@ -5,7 +5,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"os"
 )
 
@@ -45,7 +44,6 @@ func (im *ImageReader) validate() (*Chunk, error) {
 
 	header = make([]byte, 8)
 	copy(header, "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a")
-	fmt.Println("diao")
 
 	err = binary.Read(im.reader, binary.BigEndian, &buf)
 	binary.BigEndian.PutUint64(h, buf)
